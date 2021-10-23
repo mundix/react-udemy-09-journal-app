0440f08f3f37f1e371f334faae1cacda2c460bbd
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-23 09:41:48 -0400

    Created NothingComponent and style files

--- src/component/journal/JournalScreen.js
+++ src/component/journal/JournalScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
+import { NothingSelected } from './NothingSelected';
 
 export const JournalScreen = () => {
     return (
         <div className="journal__main-content">
             <Sidebar />
+            <main>
+                <NothingSelected/>
+            </main>
         </div>
     )
 }
