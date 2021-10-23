2149230e28de172625d826688c192daffa58b7c5
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-23 09:33:49 -0400

    Refactored JournalEntry sass and component

--- src/component/journal/JournalEntry.js
+++ src/component/journal/JournalEntry.js
@@ -2,25 +2,26 @@ import React from 'react'
 
 export const JournalEntry = () => {
     return (
-        <div className="journal__entry">
-
-            <div className="journal_entry-picture"
+        <div className="journal__entry pointer">
+            
+            <div 
+                className="journal__entry-picture"
                 style={{
                     backgroundSize: 'cover',
-                    backgroundImage: 'url(https://i.insider.com/520a7f3a6bb3f73f29000001?width=1000&format=jpeg&auto=webp)'
+                    backgroundImage: 'url(https://earthsky.org/upl/2018/12/comet-wirtanen-Jack-Fusco-dec-2018-Anza-Borrego-desert-CA-e1544613895713.jpg)'
                 }}
-            >
-            </div>
+            ></div>
 
             <div className="journal__entry-body">
-                <p className="journal_entry-title">
-                    Un nuevo dia
+                <p className="journal__entry-title">
+                    Un nuevo día
                 </p>
-                <p className="journal_entry-content">
-                    Veniam magna anim quis enim sit non esse.
+                <p className="journal__entry-content">
+                    Reprehenderit id in duis consectetur deserunt veniam fugiat.
                 </p>
             </div>
-            <div className="journal_entry-date-box">
+
+            <div className="journal__entry-date-box">
                 <span>Monday</span>
                 <h4>28</h4>
             </div>
