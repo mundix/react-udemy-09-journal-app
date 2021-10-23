e19484f6bc25e2b9d8bd917162b5a6f7769d6296
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-23 08:32:47 -0400

    Create JournalEntry component

---
+++ src/component/journal/JournalEntries.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const JournalEntries = () => {
+
+    const entries = [1,2,3,4,5,6,7,8,9,10];
+
+    return (
+        <div className="jounrla_entries">
+            
+        </div>
+    )
+}
