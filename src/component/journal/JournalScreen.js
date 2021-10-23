1f3d7a0d6d1fb838e76ea1a3123e20b784e0d60c
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-23 08:25:59 -0400

    Working with sidebar add styling and component sass file

--- src/component/journal/JournalScreen.js
+++ src/component/journal/JournalScreen.js
@@ -4,7 +4,6 @@ import { Sidebar } from './Sidebar';
 export const JournalScreen = () => {
     return (
         <div className="journal__main-content">
-            <h1>Main Content</h1>
             <Sidebar />
         </div>
     )
--- src/component/journal/Sidebar.js
+++ src/component/journal/Sidebar.js
@@ -4,10 +4,20 @@ export const Sidebar = () => {
     return (
         <aside className="journal__sidebar"> 
             <div className="journal__sidebar-navbar">
-                <h3>
-                    <span>Edmundo!</span>
+                <h3 className="mt-5">
+                    <i className="far fa-moon"></i>
+                    <span> Edmundo!</span>
                 </h3>
+                <button className="btn">
+                    Logout
+                </button>
             </div>
+
+            <div className="journal__new-entry">
+                <i className="far fa-calendar-plus fa-5x"></i>
+                <p>New Entry</p>
+            </div>
+
         </aside>
     )
 }
