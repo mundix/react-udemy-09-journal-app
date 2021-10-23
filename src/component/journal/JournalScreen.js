9570fcd61c0f68d14ba2732ff4c79259f3a80be5
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-23 09:49:06 -0400

    Created Notes styles and component

--- src/component/journal/JournalScreen.js
+++ src/component/journal/JournalScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { NoteScreen } from '../notes/NoteScreen';
 import { Sidebar } from './Sidebar';
-import { NothingSelected } from './NothingSelected';
+// import { NothingSelected } from './NothingSelected';
 
 export const JournalScreen = () => {
     return (
         <div className="journal__main-content">
             <Sidebar />
             <main>
-                <NothingSelected/>
+                <NoteScreen/>
+                {/* <NothingSelected/> */}
             </main>
         </div>
     )
---
+++ src/component/notes/NoteScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NoteAppBar } from './NoteAppBar';
+
+export const NoteScreen = () => {
+    return (
+        <div className="notes__main-content">
+            <NoteAppBar/>
+        </div>
+    )
+}
