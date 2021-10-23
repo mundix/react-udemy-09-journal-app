be311ebd744db8032d76c78a9df6b568d1b81b6e
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-23 10:08:43 -0400

    Ready NotScreen Layout and Style

--- src/component/notes/NoteScreen.js
+++ src/component/notes/NoteScreen.js
@@ -4,7 +4,25 @@ import { NoteAppBar } from './NoteAppBar';
 export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
-            <NoteAppBar/>
+            <NoteAppBar />
+            <div className="notes__content">
+                <input
+                    type="text"
+                    className="notes__title-input"
+                    placeholder="Some awesome title"
+                />
+
+                <textarea
+                    placeholder="What happend today"
+                    className="notes__textarea"
+                ></textarea>
+                <div className="notes__image">
+                    <img
+                        src="https://bigthink.com/wp-content/uploads/2021/10/greg-rakozy-oMpAz-DN-9I-unsplash-e1633352959373.jpg?lb=1536,864"
+                        alt="notes__image"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
