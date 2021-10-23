ef2578af7ce284f3e43dc1c6cbe7994fdabc874e
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-23 07:30:35 -0400

    Add google sign in

--- src/component/login/LoginScreen.js
+++ src/component/login/LoginScreen.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const LoginScreen = () => {
     return (
         <>
             <h3>Login</h3>
             <form >
-                <input 
-                    type="text" 
+                <input
+                    type="text"
                     placeholder="email"
                     name="email"
-                    />
-                <input 
-                    type="password" 
+                />
+                <input
+                    type="password"
                     placeholder="password"
                     name="password"
-                    />
+                />
 
                 <button type='submit'>
                     Login
@@ -22,7 +23,24 @@ export const LoginScreen = () => {
 
                 <hr />
 
-
+                <div>
+                    <p>Login With Social Network</p>
+                </div>
+                <div
+                    className="google-btn"
+                >
+                    <div className="google-icon-wrapper">
+                        <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                    </div>
+                    <p className="btn-text">
+                        <b>Sign in with google</b>
+                    </p>
+                </div>
+                <Link
+                    to='/auth/register'
+                >
+                    Create New Account
+                </Link>
             </form>
         </>
     )
