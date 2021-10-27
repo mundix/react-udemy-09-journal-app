69100290a90f996d59fe78c539281b82e6007b8c
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-27 05:05:31 -0400

    Settle on Redux dev tool on chrome

--- src/store/store.js
+++ src/store/store.js
@@ -9,4 +9,7 @@ const reducers = combineReducers({
 });
 
 // Lo importaremos en el punto mas alto de la app , JournalApp
-export const store = createStore(reducers);
+export const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
