4b508f37d0d3470d59201572ddccb454434faf93
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-29 04:54:38 -0400

    Created uiRedcuer and insert ui types

--- src/types/types.js
+++ src/types/types.js
@@ -2,4 +2,7 @@
 export const types = {
     login: '[Auth] Login',
     logout: '[Auth] Logout',
+     // Esto es para el reducer de ui 
+     uiSetError: '[UI] Set Error',
+     uiRemoveError: '[UI] Remove Error'
 }
